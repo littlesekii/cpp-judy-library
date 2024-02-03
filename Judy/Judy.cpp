d07ec8180a475a131fcddd9f79d86ab749164e29
littlesekii <littlesekii@gmail.com>
2024-02-03 17:13:59 -0300

    changed read and write functions to header file

--- Judy/Judy.cpp
+++ Judy/Judy.cpp
@@ -1,41 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Judy.h"
-#include <fstream>
 #include <cstring>
-
-using std::ifstream;
-using std::ofstream;
-using std::ios;
 
 Judy::Judy(const char* fileName)
 {
 	strcpy(this->fileName, fileName);
-}
-
-template<typename T>
-bool Judy::write(T* reg, int size)
-{
-	ofstream fout;
-	fout.open(this->fileName, ios::binary | ios::trunc);
-
-	if (fout.is_open())
-	{
-		fout.write((char*)reg, sizeof(T) * size);
-	}
-	fout.close();
-	return true;
-}
-
-template<typename T>
-bool Judy::read(T* reg, int size)
-{
-	ifstream fin;
-	fin.open(this->fileName, ios::binary | ios::in);
-
-	if (fin.is_open())
-	{
-		fin.read((char*)reg, sizeof(T) * size);
-	}
-	fin.close();
-	return true;
 }
